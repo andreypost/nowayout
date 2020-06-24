690912590998b7e7e618274c62e51412364d26e7
andrey post <postoliuk.av@gmail.com>
2020-06-24 13:59:45 +0300

    Updates

---
+++ precache-manifest.181ff16bf97fc86d4d75bec5174ec0b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb87b780c9fd99e401f4bc6e0e616cab",
+    "url": "/nowayout/index.html"
+  },
+  {
+    "revision": "f959ac6af18ea93d8500",
+    "url": "/nowayout/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "075dbbdb84de31e03754",
+    "url": "/nowayout/static/js/2.178713cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nowayout/static/js/2.178713cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f959ac6af18ea93d8500",
+    "url": "/nowayout/static/js/main.899ed68b.chunk.js"
+  },
+  {
+    "revision": "5ba870c0b48f4bb896ba",
+    "url": "/nowayout/static/js/runtime-main.48ab8189.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nowayout/static/media/logo.5d5d9eef.svg"
+  }
+]);
